ce6b61d5861e138229acbd679e640f2ad1397690
Ezekiel Victor <codeepoch@gmail.com>
2023-06-20 09:04:09 +0100

    initial commit worked on seo

--- src/components/visitors/navigation/NavBar.tsx
+++ src/components/visitors/navigation/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     <nav className="flex fixed top-0 w-screen backdrop-blur-lg z-10 items-center">
       <div className="flex-1 flex items-start">
-        <Link className="flex items-center w-fit justify-center h-full" href="/">
+        <Link className="flex items-center w-fit p-3 justify-center h-full" href="/">
           <Image className="max-w-[3rem]" width={100} height={60} src="/icon_black.svg" alt="mountain scape" />
         </Link>
       </div>
