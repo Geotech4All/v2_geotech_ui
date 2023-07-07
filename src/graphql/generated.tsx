c95f19829c3fc6baee4489e27c81a8dce86f5afc
Ezekiel Victor <codeepoch@gmail.com>
2023-07-07 08:45:44 +0100

    Fixed type issue in generated types

--- src/graphql/generated.tsx
+++ src/graphql/generated.tsx
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 import * as Urql from "urql";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
+export type Exact<T extends { [key: string]: any }> = {
   [K in keyof T]: T[K];
 };
 export type MakeOptional<T, K extends keyof T> =
@@ -11,7 +11,7 @@ export type MakeOptional<T, K extends keyof T> =
 export type MakeMaybe<T, K extends keyof T> =
   & Omit<T, K>
   & { [SubKey in K]: Maybe<T[SubKey]> };
-export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> =
+export type MakeEmpty<T extends { [key: string]: any }, K extends keyof T> =
   { [_ in K]?: never };
 export type Incremental<T> =
   | T
